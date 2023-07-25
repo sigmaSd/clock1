33b808b534000e8df524302259f83bd23f733c63
Nbiba Bedis <bedisnbiba@gmail.com>
2023-07-25 09:41:31 +0100

    js clock

---
+++ index.js
@@ -0,0 +1,46 @@
+const p1 = document.getElementById("p1")
+const p2 = document.getElementById("p2")
+if (!p1 || !p2) { throw "" }
+
+
+let time1 = 10 * 60
+let time2 = 10 * 60
+let id1
+let id2
+let turn
+
+p1.innerText = `${formatTime(time1)}`;
+p1.onclick = () => {
+  if (time2 == 0) { return }
+  if (turn == "p2") { return }
+  turn = "p2"
+  clearInterval(id1)
+  p1.style.backgroundColor = ""
+  p2.style.backgroundColor = "#38800e"
+  id2 = setInterval(() => {
+    p2.innerText = `${formatTime(time2--)}`;
+  }, 1000)
+
+}
+
+p2.innerText = `${formatTime(time2)}`;
+p2.onclick = () => {
+  if (time1 == 0) { return }
+  if (turn == "p1") { return }
+  turn = "p1"
+  clearInterval(id2)
+  p2.style.backgroundColor = ""
+  p1.style.backgroundColor = "#38800e"
+  id1 = setInterval(() => {
+    p1.innerText = `${formatTime(time1--)}`;
+  }, 1000)
+
+
+}
+
+function formatTime(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = seconds % 60;
+  return `${minutes.toString().padStart(2, "0")}:${remainingSeconds.toString().padStart(2, "0")}`;
+}
+
